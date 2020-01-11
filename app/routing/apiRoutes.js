80ee25e8ed0d0b1f50b37c3dbf79bf3db5d46406
Alyssa Albright <alyssaalbright5050@gmail.com>
2020-01-10 21:51:32 -0600

    adjusting code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,18 @@
-function apiRoutes(app) {
-    // ---------------------------------------
-    // App setup - requirments
-    // ---------------------------------------
-    var express = require("express");
-    var bodyParser = require("body-parser");
-    var path = require("path");
+var userdata = require("../app/data/friends");
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.text());
-    app.use(bodyParser.json({
-        type: "application/vnd.api+json"
-    }));
+module.exports = function(app){  //added app parameter
+
+app.get("/api/friends", function (req, res) {
+    res.json(userdata);  //enter parameter This has to connect to the variable object
+});
+
+
+app.post("/api/friends", function (req, res) {
+    res.json();
+});
+}
+
 
     
 
-    var friendsMaker = require("../data/friends.js");
-    var friends = new friendsMaker();
-
-    app.get("/api/friends", function (req, res) {
-
-        res.json(friends.showFriends());
-    });
-
-    app.post("/api/friends", function (req, res) {
-
-        
-        var newRequest = req.body;
-        
-
-        var newFriend = new friendsMaker(newRequest.name, newRequest.picture, newRequest.survey);
-        newFriend.newFriend();
-        console.log(newFriend.closestMatch);
-        res.send(newFriend.closestMatch);
-    });
-}
-
-module.exports = apiRoutes;
+   
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,28 +1,17 @@
-function htmlRoutes(app) {
-    var express = require("express");
-    var bodyParser = require("body-parser");
-    var path = require("path");
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.text());
-    app.use(bodyParser.json({
-        type: "application/vnd.api+json"
-    }));
+// Basic route that sends the user first to the AJAX Page
+module.exports = function(app){
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  
+  //add
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
 
-    app.get("/", function (req, res) {
-
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-
-    app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
-
-    app.use(express.static(path.join(__dirname, '../public/assets/')));
-
+  });
 }
-
-module.exports = htmlRoutes;
